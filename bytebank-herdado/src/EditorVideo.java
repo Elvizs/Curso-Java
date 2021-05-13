45a63b1407d888c8cbbf791192f3417c15dc014d
Elvizs <cassiano.elvis30@gmail.com>
2021-05-13 15:17:54 -0300

    Initial commit

---
+++ bytebank-herdado/src/EditorVideo.java
@@ -0,0 +1,10 @@
+
+public class EditorVideo extends Funcionario {
+	
+	public double getBonificacao(){
+		System.out.println
+		("Chamando o método de bonificação de Editor de video");
+		return + 150;
+	}
+
+}
